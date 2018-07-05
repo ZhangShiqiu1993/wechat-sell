9b40994362413cb468680408894c63c4e1db6b60
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-07-04 22:59:28 -0700

    order dto

---
+++ src/main/java/com/imooc/dto/OrderDTO.java
@@ -0,0 +1,31 @@
+package com.imooc.dto;
+
+import com.imooc.dataobject.OrderDetail;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+public class OrderDTO {
+
+    private String orderId;
+
+    private String buyerName;
+
+    private String buyerPhone;
+
+    private String buyerAddress;
+
+    private String buyerOpenid;
+
+    private BigDecimal orderAmount;
+
+    private Integer orderStatus;
+
+    private Integer payStatus;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private List<OrderDetail> orderDetailList;
+}
