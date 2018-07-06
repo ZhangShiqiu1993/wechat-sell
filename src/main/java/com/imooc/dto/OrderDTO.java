af8b684272cbae30979815eb2968c33ee31316d8
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-07-06 00:05:46 -0700

    create order

--- src/main/java/com/imooc/dto/OrderDTO.java
+++ src/main/java/com/imooc/dto/OrderDTO.java
@@ -1,10 +1,13 @@
 package com.imooc.dto;
 
 import com.imooc.dataobject.OrderDetail;
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
+@Data
 public class OrderDTO {
 
     private String orderId;
