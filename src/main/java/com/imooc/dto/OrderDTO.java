a4411540bd2c2548e3e796971bc1978ef9ec6967
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-07-10 22:01:05 -0700

    json serializer

--- src/main/java/com/imooc/dto/OrderDTO.java
+++ src/main/java/com/imooc/dto/OrderDTO.java
@@ -1,6 +1,9 @@
 package com.imooc.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.imooc.dataobject.OrderDetail;
+import com.imooc.utils.serializer.Date2LongSerializer;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -8,6 +11,8 @@ import java.util.Date;
 import java.util.List;
 
 @Data
+//@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderDTO {
 
     private String orderId;
@@ -26,9 +31,11 @@ public class OrderDTO {
 
     private Integer payStatus;
 
+    @JsonSerialize(using = Date2LongSerializer.class)
     private Date createTime;
 
+    @JsonSerialize(using = Date2LongSerializer.class)
     private Date updateTime;
 
-    private List<OrderDetail> orderDetailList;
+    List<OrderDetail> orderDetailList;
 }
