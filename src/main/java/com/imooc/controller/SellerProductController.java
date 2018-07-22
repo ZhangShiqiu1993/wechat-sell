d7a0d0ef5b9514002a057478c52e3ce0bd340b40
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-07-21 23:18:10 -0700

    save product change

--- src/main/java/com/imooc/controller/SellerProductController.java
+++ src/main/java/com/imooc/controller/SellerProductController.java
@@ -3,18 +3,23 @@ package com.imooc.controller;
 import com.imooc.dataobject.ProductCategory;
 import com.imooc.dataobject.ProductInfo;
 import com.imooc.exception.SellException;
+import com.imooc.form.ProductForm;
 import com.imooc.service.CategoryService;
 import com.imooc.service.ProductService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
 
@@ -80,5 +85,29 @@ public class SellerProductController {
         List<ProductCategory> categoryList = categoryService.findAll();
         map.put("categoryList", categoryList);
         return new ModelAndView("product/index", map);
+    }
+
+    @PostMapping("/save")
+    public ModelAndView save(@Valid ProductForm form,
+                             BindingResult bindingResult,
+                             Map<String, Object> map) {
+        if (bindingResult.hasErrors()) {
+            map.put("msg", bindingResult.getFieldError().getDefaultMessage());
+            map.put("url", "/sell/seller/product/index");
+            return new ModelAndView("common/error", map);
+        }
+        
+        try {
+            ProductInfo productInfo = productService.findOne(form.getProductId());
+            BeanUtils.copyProperties(form, productInfo);
+            productService.save(productInfo);
+        } catch (SellException e) {
+            map.put("msg", e.getMessage());
+            map.put("url", "/sell/seller/product/index");
+            return new ModelAndView("common/error", map);
+        }
+
+        map.put("url", "/sell/seller/product/list");
+        return new ModelAndView("common/success", map);
     }
 }
