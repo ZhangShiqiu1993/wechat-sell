79002df094d296e8ad3bfa254fd3e52f506aab27
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-07-21 20:54:12 -0700

    get Product Status Enum

--- src/main/java/com/imooc/dataobject/ProductInfo.java
+++ src/main/java/com/imooc/dataobject/ProductInfo.java
@@ -1,13 +1,19 @@
 package com.imooc.dataobject;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.imooc.enums.ProductStatusEnum;
+import com.imooc.utils.EnumUtil;
 import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Entity
 @Data
+@DynamicUpdate
 public class ProductInfo {
     @Id
     private String productId;
@@ -26,5 +32,12 @@ public class ProductInfo {
 
     private Integer categoryType;
 
+    private Date createTime;
 
+    private Date updateTime;
+
+    @JsonIgnore
+    public ProductStatusEnum getProductStatusEnum() {
+        return EnumUtil.getByCode(productStatus, ProductStatusEnum.class);
+    }
 }
