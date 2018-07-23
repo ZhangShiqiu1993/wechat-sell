c4965279eb16eb4053909171a58beac5866dd41d
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-07-22 23:12:49 -0700

    templateId

--- src/main/java/com/imooc/config/WechatAccountConfig.java
+++ src/main/java/com/imooc/config/WechatAccountConfig.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Data
 @Component
 @ConfigurationProperties(prefix = "wechat")
@@ -24,4 +26,6 @@ public class WechatAccountConfig {
     private String keyPath;
 
     private String notifyUrl;
+
+    private Map<String, String> templateId;
 }
