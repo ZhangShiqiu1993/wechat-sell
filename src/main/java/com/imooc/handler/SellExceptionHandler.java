9b569f068367bc064a887f34b5950c1d7e0ba463
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-07-22 23:24:03 -0700

    handler Seller Exception

--- src/main/java/com/imooc/handler/SellExceptionHandler.java
+++ src/main/java/com/imooc/handler/SellExceptionHandler.java
@@ -1,10 +1,16 @@
 package com.imooc.handler;
 
+import com.imooc.VO.ResultVO;
 import com.imooc.config.ProjectUrlConfig;
+import com.imooc.exception.SellException;
 import com.imooc.exception.SellerAuthorizeException;
+import com.imooc.utils.ResultVOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
@@ -13,6 +19,7 @@ public class SellExceptionHandler {
     private ProjectUrlConfig projectUrlConfig;
 
     @ExceptionHandler(value = SellerAuthorizeException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
     public ModelAndView handlerAuthorizeException() {
         String redirect = "redirect:"
                             .concat(projectUrlConfig.getWechatOpenAuthorize())
@@ -22,4 +29,11 @@ public class SellExceptionHandler {
                             .concat("/sell/seller/login");
         return new ModelAndView(redirect);
     }
+
+    @ExceptionHandler(value = SellException.class)
+    @ResponseBody
+    public ResultVO handlerSellerException(SellException e) {
+        return ResultVOUtil.error(e.getCode(), e.getMessage());
+    }
+
 }
