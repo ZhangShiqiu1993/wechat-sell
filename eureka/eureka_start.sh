cf048083122610779990f6ced727250fc051bc94
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-07-24 21:22:40 -0700

    eureka

--- eureka/eureka_start.sh
+++
@@ -1,2 +0,0 @@
-#nohup java -jar target/eureka-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
-java -jar target/eureka-0.0.1-SNAPSHOT.jar 
---
+++ start_eureka.sh
@@ -0,0 +1,2 @@
+#nohup java -jar eureka/target/eureka-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+java -jar eureka/target/eureka-0.0.1-SNAPSHOT.jar 
