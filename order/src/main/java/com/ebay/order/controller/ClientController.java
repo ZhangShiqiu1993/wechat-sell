6e594d268b683c7f9a4dc9b9f8fcd14dfd76b4d9
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-07-24 23:58:28 -0700

    load balancer client

---
+++ order/src/main/java/com/ebay/order/controller/ClientController.java
@@ -0,0 +1,27 @@
+package com.ebay.order.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class ClientController {
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/getProductMsg")
+    public String getProductMsg() {
+        RestTemplate restTemplate = new RestTemplate();
+        ServiceInstance serviceInstance = loadBalancerClient.choose("PRODUCT");
+        String url = String.format("http://%s:%s", serviceInstance.getHost(), serviceInstance.getPort()) + "/msg";
+        String response = restTemplate.getForObject(url, String.class);
+
+        log.info("response={}", response);
+        return response;
+    }
+}
