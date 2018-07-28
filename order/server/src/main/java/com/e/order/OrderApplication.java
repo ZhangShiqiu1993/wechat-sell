e72e60efa783290f4f44fa2f7b01e80fb15a4eaa
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-07-27 18:40:17 -0700

    order模块化

---
+++ order/server/src/main/java/com/e/order/OrderApplication.java
@@ -0,0 +1,16 @@
+package com.e.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.e.product.client")
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+}
