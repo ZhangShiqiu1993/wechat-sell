6135f07d08fe33503b8ccf65fb8a55b2815b6030
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-07-30 20:15:10 -0700

    spring cloud stream reply message

--- order/server/src/main/java/com/e/order/message/StreamReceiver.java
+++ order/server/src/main/java/com/e/order/message/StreamReceiver.java
@@ -4,6 +4,7 @@ import com.e.order.dto.OrderDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,7 +12,14 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class StreamReceiver {
     @StreamListener(StreamClient.INPUT)
-    public void process(OrderDTO message) {
+    @SendTo(StreamClient.INPUT2)
+    public String process(OrderDTO message) {
         log.info("StreamReceiver: {}", message);
+        return "received.";
+    }
+
+    @StreamListener(StreamClient.INPUT2)
+    public void process2(String message) {
+        log.info("StreamReceiver2: {}", message);
     }
 }
