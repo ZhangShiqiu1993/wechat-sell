813eb55130c7074f7d514943cfcc8deb90848c11
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-07-30 23:03:37 -0700

    store product info to redis

--- order/server/src/main/java/com/e/order/message/ProductInfoReceiver.java
+++ order/server/src/main/java/com/e/order/message/ProductInfoReceiver.java
@@ -5,14 +5,25 @@ import com.e.product.common.ProductInfoOutput;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class ProductInfoReceiver {
+
+    private static final String PRODUCT_STOCK_TEMPLATE = "product_stock_%s";
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @RabbitListener(queuesToDeclare = @Queue("productInfo"))
     public void process(String message) {
         ProductInfoOutput productInfoOutput = (ProductInfoOutput) JsonUtil.fromJson(message, ProductInfoOutput.class);
         log.info("从队列【{}】接收到消息: {}", "productInfo", productInfoOutput);
+
+        stringRedisTemplate.opsForValue().set(String.format(PRODUCT_STOCK_TEMPLATE, productInfoOutput.getProductId())
+                , String.valueOf(productInfoOutput.getProductStock()));
     }
 }
