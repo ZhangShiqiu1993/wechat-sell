962562ae07352f45d50ee4a03a600e2baf504b70
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-07-30 20:08:56 -0700

    order dto

--- order/server/src/main/java/com/e/order/controller/SendMessageController.java
+++ order/server/src/main/java/com/e/order/controller/SendMessageController.java
@@ -1,12 +1,11 @@
 package com.e.order.controller;
 
+import com.e.order.dto.OrderDTO;
 import com.e.order.message.StreamClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
 
 @RestController
 public class SendMessageController {
@@ -16,8 +15,8 @@ public class SendMessageController {
 
     @GetMapping("/sendMessage")
     public void process() {
-        String message = "now " + new Date();
-        streamClient.output().send(MessageBuilder.withPayload(message).build());
-
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setOrderId("123456");
+        streamClient.output().send(MessageBuilder.withPayload(orderDTO).build());
     }
 }
--- order/server/src/main/java/com/e/order/message/StreamReceiver.java
+++ order/server/src/main/java/com/e/order/message/StreamReceiver.java
@@ -1,5 +1,6 @@
 package com.e.order.message;
 
+import com.e.order.dto.OrderDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class StreamReceiver {
     @StreamListener(StreamClient.INPUT)
-    public void process(Object message) {
+    public void process(OrderDTO message) {
         log.info("StreamReceiver: {}", message);
     }
 }
