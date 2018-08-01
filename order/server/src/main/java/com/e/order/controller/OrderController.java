1d19806b0a9067222afde5e17d84a7b973710cb1
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-07-31 23:12:27 -0700

    order finish

--- order/server/src/main/java/com/e/order/controller/OrderController.java
+++ order/server/src/main/java/com/e/order/controller/OrderController.java
@@ -14,16 +14,13 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by 廖师兄
- * 2017-12-10 16:36
- */
 @RestController
 @RequestMapping("/order")
 @Slf4j
@@ -32,13 +29,6 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
-    /**
-     * 1. 参数检验
-     * 2. 查询商品信息(调用商品服务)
-     * 3. 计算总价
-     * 4. 扣库存(调用商品服务)
-     * 5. 订单入库
-     */
     @PostMapping("/create")
     public ResultVO<Map<String, String>> create(@Valid OrderForm orderForm,
                            BindingResult bindingResult) {
@@ -59,5 +49,10 @@ public class OrderController {
         Map<String, String> map = new HashMap<>();
         map.put("orderId", result.getOrderId());
         return ResultVOUtil.success(map);
+    }
+
+    @PostMapping("/finish")
+    public ResultVO<OrderDTO> finish(@RequestParam("orderId") String orderId) {
+        return ResultVOUtil.success(orderService.finish(orderId));
     }
 }
--- order/server/src/main/java/com/e/order/service/impl/OrderServiceImpl.java
+++ order/server/src/main/java/com/e/order/service/impl/OrderServiceImpl.java
@@ -5,6 +5,8 @@ import com.e.order.dataobject.OrderMaster;
 import com.e.order.dto.OrderDTO;
 import com.e.order.enums.OrderStatusEnum;
 import com.e.order.enums.PayStatusEnum;
+import com.e.order.enums.ResultEnum;
+import com.e.order.exception.OrderException;
 import com.e.order.repository.OrderDetailRepository;
 import com.e.order.repository.OrderMasterRepository;
 import com.e.order.service.OrderService;
@@ -15,10 +17,13 @@ import com.e.product.common.ProductInfoOutput;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -38,6 +43,7 @@ public class OrderServiceImpl implements OrderService {
     private ProductClient productClient;
 
     @Override
+    @Transactional
     public OrderDTO create(OrderDTO orderDTO) {
         String orderId = KeyUtil.genUniqueKey();
 
@@ -81,4 +87,39 @@ public class OrderServiceImpl implements OrderService {
         orderMasterRepository.save(orderMaster);
         return orderDTO;
     }
+
+
+    @Override
+    @Transactional
+    public OrderDTO finish(String orderId) {
+        //1. 先查询订单
+        Optional<OrderMaster> orderMasterOptional = orderMasterRepository.findById(orderId);
+        if (!orderMasterOptional.isPresent()) {
+            throw new OrderException(ResultEnum.ORDER_NOT_EXIST);
+        }
+
+        //2. 判断订单状态
+        OrderMaster orderMaster = orderMasterOptional.get();
+        if (OrderStatusEnum.NEW.getCode() != orderMaster.getOrderStatus()) {
+            throw new OrderException(ResultEnum.ORDER_STATUS_ERROR);
+        }
+
+        //3. 修改订单状态为完结
+        orderMaster.setOrderStatus(OrderStatusEnum.FINISHED.getCode());
+        orderMasterRepository.save(orderMaster);
+
+        //查询订单详情
+        List<OrderDetail> orderDetailList = orderDetailRepository.findByOrderId(orderId);
+        if (CollectionUtils.isEmpty(orderDetailList)) {
+            throw new OrderException(ResultEnum.ORDER_DETAIL_NOT_EXIST);
+        }
+
+        OrderDTO orderDTO = new OrderDTO();
+        BeanUtils.copyProperties(orderMaster, orderDTO);
+        orderDTO.setOrderDetailList(orderDetailList);
+
+        return orderDTO;
+    }
+
+
 }
