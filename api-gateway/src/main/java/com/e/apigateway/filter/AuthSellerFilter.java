7f67757cc5615d4c0678981fa5b9034fa9eab8a8
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-08-02 00:02:14 -0700

    seller filter

---
+++ api-gateway/src/main/java/com/e/apigateway/filter/AuthSellerFilter.java
@@ -0,0 +1,58 @@
+package com.e.apigateway.filter;
+
+import com.e.apigateway.constant.RedisConstant;
+import com.e.apigateway.utils.CookieUtil;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+@Component
+public class AuthSellerFilter extends ZuulFilter {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+
+        return "/order/order/finish".equals(request.getRequestURI());
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+
+        Cookie cookie = CookieUtil.get(request, "token");
+        if (cookie == null
+                || StringUtils.isEmpty(cookie.getValue())
+                || StringUtils.isEmpty(stringRedisTemplate.opsForValue().get(String.format(RedisConstant.TOKEN_TEMPLATE, cookie.getValue())))) {
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        }
+
+        return null;
+    }
+}
