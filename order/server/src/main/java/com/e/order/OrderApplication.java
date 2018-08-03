aad88bcf06ef0143a4abcc496b59d02be17907cb
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-08-02 22:15:56 -0700

    Spring Cloud Application

--- order/server/src/main/java/com/e/order/OrderApplication.java
+++ order/server/src/main/java/com/e/order/OrderApplication.java
@@ -1,17 +1,15 @@
 package com.e.order;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 
-@SpringBootApplication
-@EnableDiscoveryClient
+//@SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableCircuitBreaker
+@SpringCloudApplication
 @EnableFeignClients(basePackages = "com.e.product.client")
-@EnableCircuitBreaker
 public class OrderApplication {
 
 	public static void main(String[] args) {
