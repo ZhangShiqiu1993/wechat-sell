ba0a7009ba213dd7e877176c6ec22e68141032df
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-08-02 22:40:53 -0700

    断路器模式

--- order/server/src/main/java/com/e/order/controller/HystrixController.java
+++ order/server/src/main/java/com/e/order/controller/HystrixController.java
@@ -1,6 +1,8 @@
 package com.e.order.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -15,9 +17,16 @@ import java.util.Arrays;
  */
 
 @RestController
+@DefaultProperties(defaultFallback = "defaultFallback")
 public class HystrixController {
 
-    @HystrixCommand(fallbackMethod = "fallback")
+    @HystrixCommand(commandProperties = {
+// 超时等待  @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),  //设置熔断
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"), //设置熔断最小请求数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"), //设置休眠时间窗
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),
+    })
     @GetMapping("/getProductInfoList")
     public String getProductInfoList() {
         RestTemplate restTemplate = new RestTemplate();
@@ -29,5 +38,9 @@ public class HystrixController {
 
     private String fallback() {
         return "太拥挤了，请稍后再试~";
+    }
+
+    private String defaultFallback() {
+        return "默认提示：太拥挤了，请稍后再试~";
     }
 }
