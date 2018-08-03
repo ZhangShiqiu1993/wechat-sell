b376dfbc8856c4cd64a47819b143d4c9992bb124
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-08-02 22:14:59 -0700

    add hystrix

--- order/server/src/main/java/com/e/order/OrderApplication.java
+++ order/server/src/main/java/com/e/order/OrderApplication.java
@@ -2,6 +2,8 @@ package com.e.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
@@ -9,6 +11,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.e.product.client")
+@EnableCircuitBreaker
 public class OrderApplication {
 
 	public static void main(String[] args) {
