aef1ab5ba8127e24c599aab3b2558e50b12b719a
zhangshiqiu <zhangshiqiu1993@gmail.com>
2018-08-02 23:17:20 -0700

    hystrix dashboard

--- order/server/src/main/java/com/e/order/OrderApplication.java
+++ order/server/src/main/java/com/e/order/OrderApplication.java
@@ -3,6 +3,7 @@ package com.e.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 
 
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringCloudApplication
 @EnableFeignClients(basePackages = "com.e.product.client")
 @ComponentScan(basePackages = "com.e")
+@EnableHystrixDashboard
 public class OrderApplication {
 
 	public static void main(String[] args) {
